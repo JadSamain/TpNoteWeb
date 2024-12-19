8e9289d7b1421eae766247883d788ae82d880232
Jad SAMAIN <jad.samain@univ-lyon2.fr>
2024-12-19 11:34:16 +0100

    push map

--- Site/Js/map_viz.js
+++ Site/Js/map_viz.js
@@ -1,29 +1,35 @@
-// Initialisation de la carte et configuration de la vue
-var map = L.map('map').setView([45.441343, 4.386274], 14); // Latitude, Longitude, Zoom (ici : Paris)
+document.addEventListener('DOMContentLoaded', function () {
+  const jsonData = JSON.parse(localStorage.getItem('jsonData'));
 
-// Ajouter une couche de tuiles (tiles) OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// Fonction pour lire le fichier JSON et afficher les points sur la carte
-function afficherPointsSurCarte() {
-  const jsonData = localStorage.getItem('jsonData'); // Lire le JSON depuis le localStorage
-  if (!jsonData) {
-    console.error('Aucun fichier JSON trouvé dans le localStorage');
-    return;
+  if (jsonData) {
+    initializeMap(jsonData);
+  } else {
+    console.error('No JSON data found in localStorage.');
   }
+});
 
-  jsonData.forEach(row => {
-    const geocodage = row['"Géocodage xy "'];
+function initializeMap(data) {
+  const map = L.map('map').setView([46.603354, 1.888334], 6); // Initialiser la carte avec une vue centrée sur la France
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map);
+
+  data.forEach(item => {
+    // Nettoyer les clés et les valeurs des objets JSON
+    const cleanedItem = {};
+    Object.keys(item).forEach(key => {
+      const cleanedKey = key.trim();
+      cleanedItem[cleanedKey] = item[key].trim();
+    });
+
+    const geocodage = cleanedItem['Géocodage xy'];
     if (geocodage) {
-      const [lat, lng] = geocodage.split(',').map(coord => parseFloat(coord.trim()));
-      if (!isNaN(lat) && !isNaN(lng)) {
-        L.marker([lat, lng]).addTo(map);
+      const [lat, lon] = geocodage.split(',').map(coord => parseFloat(coord.trim()));
+      if (!isNaN(lat) && !isNaN(lon)) {
+        L.marker([lat, lon]).addTo(map)
+          .bindPopup(`<b>${cleanedItem['Nom du festival']}</b><br>${cleanedItem['Commune principale de déroulement']}`);
       }
     }
   });
 }
-
-// Appel de la fonction pour afficher les points sur la carte
-afficherPointsSurCarte();
